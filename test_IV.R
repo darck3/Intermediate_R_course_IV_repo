a77c88467bc3527a9b20a226f8261baa02b557ea
darck3 <ivan.vlassakev@ki.se>
2025-04-14 15:28:32 +0200

    Update test_IV.R

--- test_IV.R
+++ test_IV.R
@@ -2,7 +2,10 @@ library(tidyverse)
 library(ggplot2)
 install.packages("palmerpenguins")
 library(palmerpenguins)
+
+#load data
 data = penguins
 
+#run
 penguins %>%
   ggplot(aes(x = bill_depth_mm)) + geom_histogram()
